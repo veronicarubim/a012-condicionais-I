03df9f6af48dca34057b1ba6931c3c8167110687
veronicarubim <veronica.rubim97@gmail.com>
2022-09-06 14:28:14 -0300

    exercícios aula 12

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,20 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
 
-if(){
+let idade = Number(prompt ("Quantos anos você tem?"))
+const isTerminouEnsMedio = confirm ("Aperte OK se você terminou o ensino médio")
+const isCursandoFacul = confirm ("Aperte Ok se você está cursando faculdade")
 
-}else{
-	
+
+if(idade >= 18){console.log("A pessoa é maior de idade")
+}else{console.log("A pessoa é menor de idade")	
 }
+
+if(isTerminouEnsMedio){console.log("A pessoa terminou o ensino médio")
+}else{console.log("A pessoa não terminou o ensino médio")	
+}
+
+if(isCursandoFacul){console.log("A pessoa está cursando faculdade")
+}else{console.log("A pessoa não está cursando faculdade")
+}
+
+
+// window.alert("")
